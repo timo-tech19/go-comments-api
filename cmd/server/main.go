8396cf874188f83d8dea8d4eb311535fa91616b0
Timo Heman <timoheman16@gmail.com>
2023-06-19 13:38:55 +0100

    Created initial database migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/timo-tech19/go-comments-api/internal/db"
+)
 
 // Run - responsible for startup and instantiation
 // of our go application
 func Run() error {
 	fmt.Println("Starting app...")
+
+	db, err := db.NewDatabase()
+
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("Failed to migrate database")
+		return err
+	}
+
+	// if err := db.Ping(context.Background()); err != nil {
+	// 	return err
+	// }
+
+	fmt.Println("Database connection and ping successful")
 	return nil
 }
 
